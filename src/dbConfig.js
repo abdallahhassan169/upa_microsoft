f09e809687739d38ebaecd51669c5cf2593c4718
abdallahhassan169 <57152007+abdallahhassan169@users.noreply.github.com>
2025-05-20 10:07:52 +0300

    Update dbConfig.js change password

--- src/dbConfig.js
+++ src/dbConfig.js
@@ -2,12 +2,12 @@ import createPostgresSubscriber from "pg-listen";
 import { Pool } from "pg";
 
 export const listener = createPostgresSubscriber({
-  connectionString: "postgres://postgres:11620000@localhost:5432/first_upa_db",
+  connectionString: "postgres://postgres:password@localhost:5432/first_upa_db",
 });
 export const sourceDb = new Pool({
-  connectionString: "postgres://postgres:11620000@localhost:5432/first_upa_db",
+  connectionString: "postgres://postgres:password@localhost:5432/first_upa_db",
 });
 export const externalDb = new Pool({
   connectionString:
-    "postgres://postgres:11620000@localhost:5432/f_second_upa_db",
+    "postgres://postgres:password@localhost:5432/f_second_upa_db",
 });
